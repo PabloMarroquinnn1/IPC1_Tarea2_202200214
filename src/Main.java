982107b3d99a57376d2c27068a4d2a7ec3d0da7a
PabloMarroquinnn1 <98430342+PabloMarroquinnn1@users.noreply.github.com>
2024-02-15 17:16:09 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        ms("----Menu calculadora---- ");
+        ms("Seleccione una opción: ");
+        ms("1. Suma");
+        ms("2. Resta");
+        ms("3. Multiplicación");
+        ms("4. División");
+
+        Scanner scanner = new Scanner(System.in);
+        int opcion = scanner.nextInt();
+
+        switch (opcion) {
+            case 1:
+                ms("ingrese el primer digito: ");
+                a = scanner.nextInt();
+                ms("Ingrese el segundo digito: ");
+                b = scanner.nextInt();
+                ms("Este es el resultado de la suma: "+ (a+b));
+                break;
+            case 2:
+                ms("ingrese el primer digito: ");
+                a = scanner.nextInt();
+                ms("Ingrese el segundo digito: ");
+                b = scanner.nextInt();
+                ms("Este es el resultado de la resta: "+ (a-b));
+                break;
+            case 3:
+                ms("ingrese el primer digito: ");
+                a = scanner.nextInt();
+                ms("Ingrese el segundo digito: ");
+                b = scanner.nextInt();
+                ms("Este es el resultado de la multiplicación: "+ (a*b));
+                break;
+            case 4:
+                ms("ingrese el primer digito de la división: ");
+                a = scanner.nextInt();
+                ms("Ingrese el segundo digito: ");
+                b = scanner.nextInt();
+                ms("Este es el resultado: "+ (a/b));
+                break;
+            default:
+                ms("Opción no válida");
+        }
+    }
+
+    public static void ms(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
